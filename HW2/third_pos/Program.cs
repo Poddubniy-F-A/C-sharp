d32048bc30c71f5251a77ab2ff30a0f70b78ede8
Poddubniy_F_A <21federror12@gmail.ru>
2023-09-17 16:35:21 +0300

    fix: add replace all tasks for 1st HW into specific folder; feat: complete 2nd HW

---
+++ HW2/third_pos/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число ");
+int num_abs = Math.abs(int.Parse(Console.ReadLine()));
+
+if (num_abs < 100) {
+    Console.WriteLine("У числа отсутствует третий разряд");
+} else {
+    while (num_abs >= 1000) {
+        num_abs /= 10;
+    }
+    Console.WriteLine("Цифра в третьем разряде: {0}", num_abs % 10);
+}
---
+++ HW2/week_day/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите порядковый номер дня недели (1-7) ");
+int day_num = int.Parse(Console.ReadLine());
+
+if (day_num < 1 || day_num > 7) {
+    Console.WriteLine("Ошибка ввода");
+} else if (day_num <= 5) {
+    Console.WriteLine("Это будний день");
+} else {
+    Console.WriteLine("Это выходной");
+}
