5fc813f974b197d38a810404472a8753313705d3
Poddubniy_F_A <21federror12@gmail.ru>
2023-09-17 16:38:34 +0300

    fix: edit solves of 2nd HW

--- HW2/second_pos/Program.cs
+++ HW2/second_pos/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.Write("Введите трёхзначное число ");
 int num_abs = Math.Abs(int.Parse(Console.ReadLine()));
 
-if (num_abs >= 1000 || num_abs < 100) {
+if (num_abs > 999 || num_abs < 100) {
     Console.WriteLine("Ошибка ввода");
 } else {
     Console.WriteLine("Вторая цифра числа: {0}", (num_abs / 10) % 10);
--- HW2/third_pos/Program.cs
+++ HW2/third_pos/Program.cs
@@ -4,7 +4,7 @@ int num_abs = Math.abs(int.Parse(Console.ReadLine()));
 if (num_abs < 100) {
     Console.WriteLine("У числа отсутствует третий разряд");
 } else {
-    while (num_abs >= 1000) {
+    while (num_abs > 999) {
         num_abs /= 10;
     }
     Console.WriteLine("Цифра в третьем разряде: {0}", num_abs % 10);
