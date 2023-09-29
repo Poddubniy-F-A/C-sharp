396c1aae31a1221cad79e91f1a6264335cc04c05
Poddubniy_F_A <21federror12@gmail.ru>
2023-09-29 21:33:48 +0300

    feat: complete 6th HW

---
+++ HW6/2DLinesIntersection/Program.cs
@@ -0,0 +1,45 @@
+﻿int LINES_NUM = 2;
+int DIMENSION = 2;
+
+double[] ConvertStringToDoubleArray(string str) {
+    string[] strArray = str.Split(" ");
+    int length = strArray.Length;
+
+    double[] doubleArray = new double[length];
+    for (int i = 0; i < length; i++) {
+        doubleArray[i] = double.Parse(strArray[i]);
+    }
+    return doubleArray;
+}
+
+void Print2DLineEquation(double[] lineEquation) {
+    Console.Write("y = {0, 0:0.###}x + {1, 0:0.###}", lineEquation[0], lineEquation[1]);
+}
+
+void Print2DLinesIntersection(double[] lineEquation1, double[] lineEquation2) {
+    Console.Write("Прямые ");
+    Print2DLineEquation(lineEquation1);
+    Console.Write(" и ");
+    Print2DLineEquation(lineEquation2);
+
+    if (lineEquation1[0] == lineEquation2[0]) {
+        if (lineEquation1[1] == lineEquation2[1]) {
+            Console.Write(" совпадают");
+        } else {
+            Console.Write(" параллельны");
+        }
+    } else {
+        double x = (lineEquation2[1] - lineEquation1[1]) / (lineEquation1[0] - lineEquation2[0]);
+        Console.Write(" пересекаются в точке ({0, 0:0.###}, {1, 0:0.###})", x, lineEquation1[0] * x + lineEquation1[1]);
+    }
+}
+
+Console.Write($"Введите {LINES_NUM * DIMENSION} числа ");
+double[] values = ConvertStringToDoubleArray(Console.ReadLine());
+double[][] lineEquations = new double[LINES_NUM][];
+for (int i = 0; i < LINES_NUM; i++) {
+    lineEquations[i] = new double[DIMENSION];
+    Array.Copy(values, DIMENSION * i, lineEquations[i], 0, DIMENSION);
+}
+
+Print2DLinesIntersection(lineEquations[0], lineEquations[1]);
---
+++ HW6/PositiveNumbersNum/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] ConvertStringToIntArray(string str) {
+    string[] strArray = str.Split(", ");
+    int length = strArray.Length;
+
+    int[] intArray = new int[length];
+    for (int i = 0; i < length; i++) {
+        intArray[i] = int.Parse(strArray[i]);
+    }
+    return intArray;
+}
+
+int PositiveNumbersNum(int[] array) {
+    int result = 0;
+    for (int i = 0; i < array.Length; i++) {
+        if (array[i] > 0) {
+            result++;
+        }
+    }
+    return result;
+}
+
+Console.Write("Введите несколько чисел через запятую ");
+int[] array = ConvertStringToIntArray(Console.ReadLine());
+
+Console.Write($"Количество положительных чисел: {PositiveNumbersNum(array)}");
