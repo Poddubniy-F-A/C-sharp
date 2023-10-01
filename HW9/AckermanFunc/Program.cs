0f9c326c5f6954150f6d5675a685015d84f1acaf
Poddubniy_F_A <21federror12@gmail.ru>
2023-10-01 22:40:13 +0300

    feat: complete 9th HW

---
+++ HW9/AckermanFunc/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] ConvertStringToIntArray(string str) {
+    string[] strArray = str.Split(" ");
+    int length = strArray.Length;
+
+    int[] intArray = new int[length];
+    for (int i = 0; i < length; i++) {
+        intArray[i] = int.Parse(strArray[i]);
+    }
+    return intArray;
+}
+
+int AckermanFunc(int m, int n) {
+    if (m == 0) {
+        return n + 1;
+    } else if (n == 0) {
+        return AckermanFunc(m - 1, 1);
+    } else {
+        return AckermanFunc(m - 1, AckermanFunc(m, n - 1));
+    }
+}
+
+Console.Write("Введите аргументы функции Аккермана (2 числа) ");
+int[] arguments = ConvertStringToIntArray(Console.ReadLine());
+
+Console.Write($"A({arguments[0]}, {arguments[1]}) = {AckermanFunc(arguments[0], arguments[1])}");
---
+++ HW9/IntervalPrinting/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] ConvertStringToIntArray(string str) {
+    string[] strArray = str.Split(" ");
+    int length = strArray.Length;
+
+    int[] intArray = new int[length];
+    for (int i = 0; i < length; i++) {
+        intArray[i] = int.Parse(strArray[i]);
+    }
+    return intArray;
+}
+
+void PrintInterval(int m, int n) {
+    if (m <= n) {
+        Console.Write($" {m}");
+        PrintInterval(m + 1, n);
+    }
+}
+
+Console.Write("Введите границы промежутка (2 числа) ");
+int[] borders = ConvertStringToIntArray(Console.ReadLine());
+
+Console.Write($"Числа на промежутке [{borders[0]}, {borders[1]}]:");
+PrintInterval(borders[0], borders[1]);
