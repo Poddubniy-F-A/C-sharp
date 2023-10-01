2b89195255de36cb45c4e3b3566a8f7f12653f9f
Poddubniy_F_A <21federror12@gmail.ru>
2023-10-01 22:39:21 +0300

    feat: complete 9th HW

---
+++ HW9/IntervalSum/Program.cs
@@ -0,0 +1,23 @@
+﻿int[] ConvertStringToIntArray(string str) {
+    string[] strArray = str.Split(" ");
+    int length = strArray.Length;
+
+    int[] intArray = new int[length];
+    for (int i = 0; i < length; i++) {
+        intArray[i] = int.Parse(strArray[i]);
+    }
+    return intArray;
+}
+
+int GetIntervalSum(int curSum, int m, int n) {
+    if (m <= n) {
+        return GetIntervalSum(curSum + m, m + 1, n);
+    } else {
+        return curSum;
+    }
+}
+
+Console.Write("Введите границы промежутка (2 числа) ");
+int[] borders = ConvertStringToIntArray(Console.ReadLine());
+
+Console.Write($"Сумма чисел на промежутке [{borders[0]}, {borders[1]}]: {GetIntervalSum(0, borders[0], borders[1])}");
